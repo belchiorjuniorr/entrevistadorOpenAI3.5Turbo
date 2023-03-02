6569ffecd410e0a13bbf1afceb2cd35456c1ac68
belchiorjuniorr <37811639+belchiorjuniorr@users.noreply.github.com>
2023-03-02 09:34:15 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 import os
 import openai
-openai.api_key = ("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") 
-openai.organization = ("org-q5F6xlw7KvehyPsmYskyqN7B") 
+openai.api_key = ("sua chave api aqui") 
+openai.organization = ("sua org openai aqui") 
 
 conversation=[{"role": "system", "content": "Quero que você atue como um entrevistador chamado José Belchior. Eu serei o candidato e você fará as perguntas da entrevista para a posição do cargo Técnico em Desenvolvimento de Sistemas. Quero que você apenas responda como o entrevistador. Não escreva toda a conversa de uma vez. Quero que você apenas faça a entrevista comigo. Faça as perguntas e espere as minhas respostas. Não escreva explicações. Faça as perguntas uma a uma, como um entrevistador faz e espere as minhas respostas. Minha primeira frase é “Oi”."}]
 
